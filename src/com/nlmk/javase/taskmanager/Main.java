4f106df33bc4820d07dd8d5411bd001069584261
Сергей Дежемесов <dserg1972@gmail.com>
2020-07-06 21:53:55 +0300

    Очистка текста.

--- src/com/nlmk/javase/taskmanager/Main.java
+++ src/com/nlmk/javase/taskmanager/Main.java
@@ -3,8 +3,7 @@ package com.nlmk.javase.taskmanager;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
-    System.out.println( "Hello, World!\n" );
+        System.out.println( "Hello, World!\n" );
     }
 }

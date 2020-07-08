7197a62f41f818aecd51c2bd95b167cdadc7db65
Сергей Дежемесов <dserg1972@gmail.com>
2020-07-08 21:07:21 +0300

    Переименован основной пакет. Добавлен пакет для классов констант. Добавлен пакет с константами для консольного модуля. Реализована обработка ключай запуска и сообщение приветствия. Добавлена генерация javadoc.

---
+++ src/com/nlmk/dezhemesov/taskmanager/Main.java
@@ -0,0 +1,66 @@
+package com.nlmk.dezhemesov.taskmanager;
+
+import static com.nlmk.dezhemesov.taskmanager.constants.Console.*;
+
+/**
+* Консольный модуль. Точка входа в приложение
+*
+*/
+
+public class Main {
+
+    /**
+     * Точка входа в приложение
+     * @param args - аргументы командной строки
+     */
+    public static void main(final String[] args) {
+        processKeys(args);
+        System.out.println("*** WELCOME TO THE TASK MANAGER ***");
+    }
+
+    /**
+     * Обработчик ключей запуска
+     * @param args аргументы командной строки
+     */
+    private static void processKeys(String[] args) {
+        if (args == null) return;
+        if (args.length < 1) return;
+        switch (args[0]) {
+            case ABOUT:
+                showAbout();
+            case HELP:
+                showHelp();
+            case VERSION:
+                showVersion();
+            default:
+                showUnknownKey(args[0]);
+        }
+    }
+
+    /**
+     *
+     */
+    private static void showAbout() {
+        System.out.println("Author: Serge Dezhemesov");
+        System.exit(0);
+    }
+
+    private static void showVersion() {
+        System.out.println("Version: 1.0.0");
+        System.exit(0);
+    }
+
+    private static void showHelp() {
+        System.out.println("Usage: java -jar taskmanager.jar [about|help|version]");
+        System.exit(0);
+    }
+
+    /**
+     * @param key значение неподдерживаемого ключа
+     */
+    private static void showUnknownKey(String key) {
+        System.out.println("Unknown key: \"" + key + "\"");
+        System.exit(1);
+    }
+
+}
---
+++ src/com/nlmk/dezhemesov/taskmanager/constants/Console.java
@@ -0,0 +1,19 @@
+package com.nlmk.dezhemesov.taskmanager.constants;
+
+/**
+ * Константные значения для использования в консольном модуле
+ */
+public class Console {
+    /**
+     * Сведения об авторе
+     */
+    public static final String ABOUT = "about";
+    /**
+     * Помощь по ключам запуска
+     */
+    public static final String HELP = "help";
+    /**
+     * Сведения о версии
+     */
+    public static final String VERSION = "version";
+}

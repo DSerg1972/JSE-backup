a14c0f595a5f570942a508564f0212e54af842c6
Сергей Дежемесов <dserg1972@gmail.com>
2020-07-08 18:20:34 +0300

    Улучшение вида

--- src/com/nlmk/javase/taskmanager/Main.java
+++ src/com/nlmk/javase/taskmanager/Main.java
@@ -2,7 +2,7 @@ package com.nlmk.javase.taskmanager;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
 
         System.out.println( "Hello, World!" );
     }
